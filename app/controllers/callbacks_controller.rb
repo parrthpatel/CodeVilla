57c25f5458f18c7066665765a336aa4c10b60435
Parth Patel <parth1634@gmail.com>
2016-02-12 12:47:31 +0530

    Update callbacks_controller.rb

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,16 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-=begin
-def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    
-    sign_in_and_redirect 'welcome/index'
-  end
-=end
 
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    #binding.pry
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
